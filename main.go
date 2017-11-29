46e897d790c8b11f0b4e2fd8281316a393d0ab33
Joshua Cork <joshua.cork@intel.com>
2017-11-29 15:56:58 -0700

    rewriting the give quiz function to facilitate timer non-blocking functionality

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import "os"
 // import "io/ioutil"
 import "bufio"
 import "strings"
+import "flag"
+import "time"
 
 func check(e error){
 	if e != nil {
@@ -12,65 +14,130 @@ func check(e error){
 	}
 }
 
-func main() {
+type QuizItem struct {
+	question 		string
+	expectedAnswer 	string
+	actualAnswer 	string
+	questionScore 	int
+}
 
-	reader := bufio.NewReader(os.Stdin)
+func calcScore (results []QuizItem) {
+	totalScore := 0
+	for _,item := range results {
+		totalScore += item.questionScore
+	} 
+	fmt.Printf("You scored %d out of a possible %d \n", totalScore, len(results))
+}
 
-	type quizItem struct {
-		question 		string
-		expectedAnswer 	string
-		actualAnswer 	string
-	}
-	
-	quiz := make([]quizItem,0)
-	score := 0
+func main() {	
 	
 	
+	fnPtr := flag.String("fileName", "problems.csv", "name of the file in root that contains the quiz")	
+	quizBegin := bufio.NewScanner(os.Stdin)
+	quiz := make([]QuizItem,0)	
 	
-	fmt.Println("Starting the read")
-	f, err := os.Open("problems.csv")
+	// Once all flags are declared, call `flag.Parse()`
+    // to execute the command-line parsing.
+    flag.Parse()
+	
+	fmt.Println("fileName:",*fnPtr)	
+	
+	f, err := os.Open(*fnPtr)
 	check(err)
 	defer f.Close()
-	
 
-	// Move back to the start of file to run method two
-	_, err = f.Seek(0, 0)
-    check(err)
-	
+	// parse file into slice of quiz objects		
 	// reads the entire file and splits it by line.
 	scanner := bufio.NewScanner(f)
 	scanner.Split(bufio.ScanLines)
-	
-	for scanner.Scan() {
 
-		
+	for scanner.Scan() {
 		splitQ := strings.Split(scanner.Text(),",")
 		theQuestion := splitQ[0]
-		fmt.Println(theQuestion)
 		theExpectedAnswer := splitQ[1]
-		theAnswer, err := reader.ReadString('\n')
-		theAnswer = strings.Replace(theAnswer, "\n", "", -1)
-
-		if err != nil {
-			println(err)
-		}
-
-		if theExpectedAnswer == theAnswer {
-			score++
-		}
-		
-		quiz = append(quiz, quizItem{
+		quiz = append(quiz, QuizItem{
 			question: theQuestion,
 			expectedAnswer: theExpectedAnswer,
-			actualAnswer: theAnswer,
-		})	
-		
-		
+			actualAnswer: "",
+			questionScore: 0,
+		})
+	}
+	
+	fmt.Println("Quiz file has been read in, are you ready to start?  Press enter to continue!")
 
+	for quizBegin.Scan() {
+		inputReceived := quizBegin.Text()
+		if inputReceived == "" {			
+			// Move back to the start of file to run method two
+			_, err = f.Seek(0, 0)
+			check(err)		
+			giveQuiz(quiz)			
+			break
+		}
+	}
 
-		// fmt.Println(scanner.Text())
-
-	  }
-	  fmt.Printf("You scored %d out of a possible %d \n", score, len(quiz))
+	
 
 }
+
+func giveQuiz(quizFile []QuizItem) {
+
+	quizTimer := time.NewTimer(time.Second * 5)	
+
+	for i, item := range quizFile {
+		fmt.Printf("Question #%d: %s\n", i+1, item.question)
+		answerCh := make(chan string)
+
+		go func() {
+			var actualAnswer string
+			fmt.Scanf("%s\n", &actualAnswer)
+		}()
+
+		select {
+		case <- quizTimer.C:
+			fmt.Printf("\nThe quiz timer has expired.\n")
+			calcScore(quizFile)
+			return		
+		case actualAnswer := <-answerCh:
+			if actualAnswer == item.expectedAnswer {
+				item.questionScore = 1
+			}
+		}
+
+	}
+
+	calcScore(quizFile)
+		
+		// for scanner.Scan() {
+		// 	select {
+		// 	case <-quizTimer.C:
+		// 		fmt.Println("quiz timer has expired.")
+		// 		return
+		// 	default:
+		// 		score := 0			
+				
+		// 		fmt.Println(theQuestion)
+				
+		// 		theAnswer, err := reader.ReadString('\n')
+		// 		theAnswer = strings.Replace(theAnswer, "\n", "", -1)
+		
+		// 		if err != nil {
+		// 			println(err)
+		// 		}
+		
+		// 		if theExpectedAnswer == theAnswer {
+		// 			score++
+		// 		}
+				
+					
+		// 	}
+	
+		// 	// fmt.Println(scanner.Text())
+	
+		// }
+		
+		// calcScore(quiz)	
+	
+
+}
+
--- myNotes.md
+++ myNotes.md
@@ -1,6 +1,26 @@
 # General Notes
 A detailed account of my experience in reverse chronological order.
 
+## Day 5:  11/21
+
+- cleaned up and got it working again.  
+- now I need to use select statements and have the timer call the results function or break out.  
+  - He covers this in his video: https://gophercises.com/exercises/quiz
+
+
+## Day 4: 11/17
+- implemented flags for choosing new quiz file
+- started implementing the timer functionality but I still have more work to do there.
+  - I need to break out the scoring feature and call that if the timer runs out and then exit the program.
+  - If they complete the quiz I need to call the scoring feature and stop the timeer. 
+
+## Day 3: 11/16/2017
+
+- I can now read in a file and print the values of that file to standard out and await a response for stanard in.
+- My code is now comparing the input from standard in to the expected answer.
+- My code accurately counts the number of correct answers and informs the user how many answer they got correct out of the total number of questions.
+
+
 ## Day 1: 11/03/2017
 
 - Leaving off here:
